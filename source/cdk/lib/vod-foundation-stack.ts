96cbb9261d17b858a7ce7a24c99f76e30c0ba635
Alrick Telfer <alrick@kingstoncloud.io>
2021-11-24 22:17:41 -0500

    (fix): Fix cors headers function

--- source/cdk/lib/vod-foundation-stack.ts
+++ source/cdk/lib/vod-foundation-stack.ts
@@ -116,11 +116,9 @@ export class VodFoundation extends cdk.Stack {
             code: cf.FunctionCode.fromInline("function handler(event)  {\
                 var response = event.response;\
                 var headers = response.headers;\
-                // If Access-Control-Allow-Origin CORS header is missing, add it.\
-                // Since JavaScript doesn't allow for hyphens in variable names, we use the dict[\"key\"] notation.\
                 if(!headers['access-control-allow-origin']) {\
-                    headers['access-control-allow-origin'] = { value: \"*\" };\
-                    console.log(\"Access-Control-Allow-Origin was missing, adding it now.\");\
+                    headers['access-control-allow-origin'] = { value: '*' };\
+                    console.log('Access-Control-Allow-Origin was missing, adding it now.');\
                 }\
                 return response;\
             }")
